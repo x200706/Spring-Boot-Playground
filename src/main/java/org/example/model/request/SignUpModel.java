15572b66122a3160d66262050373b192057f3226
Crystal Chen <crystal.chen@fullerton.com.tw>
2023-11-02 14:15:57 +0800

    Remove useless import

--- src/main/java/org/example/model/request/SignUpModel.java
+++ src/main/java/org/example/model/request/SignUpModel.java
@@ -2,13 +2,9 @@ package org.example.model.request;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-
 @Data
 public class SignUpModel {
     String username;
     String password;
     String name;
-
 }
