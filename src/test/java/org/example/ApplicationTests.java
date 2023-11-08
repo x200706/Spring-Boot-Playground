c21e33d78d762c5b03453c3caa5ac0adf66bea53
Crystal <chi200706@gmail.com>
2023-11-08 13:44:42 +0800

    add a mini test

---
+++ src/test/java/org/example/ApplicationTests.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+// JUnit 5不用寫@RunWith
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/org/example/TestForTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestForTest {
+
+    @Test
+    public void miniTest(){
+        System.out.println("Mini Test!");
+    }
+}
