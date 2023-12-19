cc060570f321f20c93db1bcdbea20e2a92699636
Crystal <chi200706@gmail.com>
2023-12-19 14:18:00 +0800

    remove useless import

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,7 @@
 package org.example;
 
-
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 // @MapperScan("org.example.mybatis")
